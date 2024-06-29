cf557d2cc1eabbae4f788e65cce895584a2d9afc
chelsofia <rachelasse01@gmail.com>
2024-06-29 06:03:48 +0100

    update

---
+++ pizzon/app/users/cart/page.js
@@ -0,0 +1,13 @@
+import Navbar from "@/app/components/navbar";
+import Footer from "@/app/components/footer";
+
+
+export default function Cart () {
+    return(
+        
+        <main>
+            <Navbar/>
+         
+        </main>
+    )
+}
